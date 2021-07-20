a6356ed70faee42f752be62ac75ecc04bec17279
pada8716 <pada8716@colorado.edu>
2021-07-20 12:12:11 -0600

    print current and Rdut

--- Micro-ohmmeter_xiao_ADS1115/Micro-ohmmeter_xiao_ADS1115.ino
+++ Micro-ohmmeter_xiao_ADS1115/Micro-ohmmeter_xiao_ADS1115.ino
@@ -8,9 +8,19 @@
 
 Adafruit_ADS1115 ads;
 
+
+
 int16_t results1;
 int16_t results2;
-float multiplier = 0.0078125F;
+float multiplier_mv = 0.0078125F;
+float multiplier_v = 0.0000078125F;
+
+float Rs = 0.2;
+
+float Vs;
+float Vdut;
+float Is;
+float Rdut;
 
 
 long timeStart;
@@ -32,11 +42,20 @@ void setup() {
 void loop() {
   timeStart = micros();
   results1 = ads.readADC_Differential_0_1();
-  timeStop = micros();
   results2 = ads.readADC_Differential_2_3();
+  timeStop = micros();
   Serial.print("time: ");
   Serial.println(timeStop - timeStart);
 
-    Serial.print("Differential_1: "); Serial.print(results1); Serial.print("("); Serial.print(results1 * multiplier); Serial.println("mV)");
-    Serial.print("Differential_2: "); Serial.print(results2); Serial.print("("); Serial.print(results2 * multiplier); Serial.println("mV)");
+    Serial.print("Differential_1: "); Serial.print(results1); Serial.print("("); Serial.print((results1 * multiplier_mv),6); Serial.println("mV)");
+    Serial.print("Differential_2: "); Serial.print(results2); Serial.print("("); Serial.print((results2 * multiplier_mv),6); Serial.println("mV)");
+
+    Vs = results1*multiplier_v;
+    Vdut = results2*multiplier_v;
+
+    Is = Vs/Rs;
+    Rdut = Vdut/Is;
+    Serial.print("Is: "); Serial.println(Is,6);
+    Serial.print("Rdut: "); Serial.println(Rdut,6);
+    
 }
